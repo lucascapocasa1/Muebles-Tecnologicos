fd26077114db4694188aa6b26e38bcd5de6fcf8a
Javier <cerdajavier177@gmail.com>
2025-05-29 22:12:33 -0300

    Guardo archivos nuevos para poder actualizar el repositorio

---
+++ tienda/static/comportamientos.js
@@ -0,0 +1,18 @@
+function redireccionarALinkedin(){
+    /*window.location.href = "https://www.linkedin.com/home"*/
+    window.location.href = "opcion1.html"
+}
+
+function redireccionarAFarmacity(){
+    window.location.href = "https://www.farmacity.com/"
+
+}
+
+/*function redireccionarALogin(){
+    window.location.href = "login2.html"
+}
+    */
+
+function redireccionarALogin() {
+    window.location.href = "/login2/";  // Redirigir a la URL correcta de Django
+}
